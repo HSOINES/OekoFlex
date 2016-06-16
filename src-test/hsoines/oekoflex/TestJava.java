094dacffff733d99d6e48f1e862f8902c5a3f77c
fr2e <info@fr2e.de>
2016-06-16 13:28:01 +0200

    sample

---
+++ src-test/hsoines/oekoflex/TestJava.java
@@ -0,0 +1,14 @@
+package hsoines.oekoflex;
+
+import org.junit.Test;
+
+/**
+ * Created by jhense on 16.06.2016.
+ */
+public class TestJava {
+    @Test
+    public void testName() throws Exception {
+        float i = (float) (1 / 3);
+        System.out.println(i);
+    }
+}
