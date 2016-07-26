51377ecf16956f5042a2162d838f5d0eafe0fcfd
fr2e <info@fr2e.de>
2016-07-26 10:41:45 +0200

    Comments

--- src/hsoines/oekoflex/marketoperator/BalancingMarketOperator.java
+++ src/hsoines/oekoflex/marketoperator/BalancingMarketOperator.java
@@ -6,24 +6,39 @@ import hsoines.oekoflex.bid.PowerPositive;
 import hsoines.oekoflex.domain.SequenceDefinition;
 import repast.simphony.engine.schedule.ScheduledMethod;
 
-public interface BalancingMarketOperator extends OekoflexAgent {
-	void addPositiveSupply(PowerPositive supply);
+/*
+	Bereinigt den Regelenergiemarkt
+	-> Empfängt Gebote als Supplies und Demands von den MarketTradern
+	-> Ermittelt die Gebote, welche angenommen werden (Preis entspricht dem des Gebots)
+	-> Notifiziert die MarketTrader über das Ergebnis ihres Angebots
 
+	Für die Diagrammanzeige werden Getter bereitgestellt
+ */
+public interface BalancingMarketOperator extends OekoflexAgent {
+
+	/*
+		Übergabe der Angebote
+	 */
+	void addPositiveSupply(PowerPositive supply);
 	void addNegativeSupply(PowerNegative supply);
 
+	/*
+		Markträumung, wird von Repast-Scheduler aufgerufen
+	 */
 	@ScheduledMethod(start = SequenceDefinition.SimulationStart, interval = SequenceDefinition.BalancingMarketInterval, priority = SequenceDefinition.BalancingMarketClearingPriority)
 	void clearMarket();
 
-
+	/*
+		Getter für Tests
+	 */
 	float getTotalClearedPositiveQuantity();
-
 	float getTotalClearedNegativeQuantity();
 
+	/*
+    	Getter für Diagramm
+ 	*/
 	float getLastPositiveAssignmentRate();
-
 	float getLastClearedNegativeMaxPrice();
-
 	float getLastNegativeAssignmentRate();
-
 	float getLastClearedPositiveMaxPrice();
 }
