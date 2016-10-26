c1457efbc2bfade1eca8ed70694dcbdc83365e70
andreas <asaelin1@stud.hs-offenburg.de>
2016-10-26 12:09:00 +0200

    ...

--- src/hsoines/oekoflex/OekoflexAgent.java
+++ src/hsoines/oekoflex/OekoflexAgent.java
@@ -1,10 +1,6 @@
 package hsoines.oekoflex;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jh
- * Date: 09/12/15
- * Time: 21:26
  * Alle Objekte, die von Repast als "Agents" verarbeitet werden.
  */
 public interface OekoflexAgent {
