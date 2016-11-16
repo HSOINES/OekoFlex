21adee793dcfdd0059e5bd80a744a6835a2ee238
andre <andre@141.79.49.101>
2016-11-16 11:30:09 +0100

    FlexPowerplant2FactoryTest works again

--- src-test/hsoines/oekoflex/builder/traderfactories/FlexPowerplant2FactoryTest.java
+++ src-test/hsoines/oekoflex/builder/traderfactories/FlexPowerplant2FactoryTest.java
@@ -1,31 +1,31 @@
-//package hsoines.oekoflex.builder.traderfactories;
-//
-//import hsoines.oekoflex.builder.OekoFlexContextBuilder;
-//import org.apache.log4j.BasicConfigurator;
-//import org.junit.Test;
-//import repast.simphony.context.DefaultContext;
-//
-//import java.io.File;
-//import java.text.DecimalFormat;
-//import java.util.Locale;
-//import java.util.Properties;
-//
-///**
-// * User: jh
-// * Date: 27/01/16
-// * Time: 22:45
-// */
-//public class FlexPowerplant2FactoryTest {
-//    static {
-//        Locale.setDefault(OekoFlexContextBuilder.defaultlocale);
-//        OekoFlexContextBuilder.defaultNumberFormat = DecimalFormat.getNumberInstance();
-//    }
-//
-//    @Test
-//    public void testIt() throws Exception {
-//        BasicConfigurator.configure();
-//        final File configDir = new File("run-config/test");
-//        Properties globalProperties = OekoFlexContextBuilder.loadProperties(configDir);
-//        FlexPowerplant2Factory.build(configDir, new DefaultContext<>(), null, null, null, globalProperties);
-//    }
-//}
+package hsoines.oekoflex.builder.traderfactories;
+
+import hsoines.oekoflex.builder.OekoFlexContextBuilder;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Test;
+import repast.simphony.context.DefaultContext;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Properties;
+
+/**
+ * User: jh
+ * Date: 27/01/16
+ * Time: 22:45
+ */
+public class FlexPowerplant2FactoryTest {
+    static {
+        Locale.setDefault(OekoFlexContextBuilder.defaultlocale);
+        OekoFlexContextBuilder.defaultNumberFormat = DecimalFormat.getNumberInstance();
+    }
+
+    @Test
+    public void testIt() throws Exception {
+        BasicConfigurator.configure();
+        final File configDir = new File("run-config/test");
+        Properties globalProperties = OekoFlexContextBuilder.loadProperties(configDir);
+        FlexPowerplant3Factory.build(configDir, new DefaultContext<>(), null, null, null, globalProperties);
+    }
+}
